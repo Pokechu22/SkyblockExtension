bfb38f147170d4c321bd81708eccc8abd97fc422
Pokechu22 <Pokechu022@gmail.com>
2014-08-01 17:34:13 -0700

    Added the inst code Readded instance code from my previous version.

--- SkyblockExtension/src/main/java/pokechu22/plugins/SkyblockExtension/SkyblockExtension.java
+++ SkyblockExtension/src/main/java/pokechu22/plugins/SkyblockExtension/SkyblockExtension.java
@@ -2,6 +2,26 @@ package pokechu22.plugins.SkyblockExtension;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**\
+ * 
+ * @author Pokechu22
+ *
+ */
 public class SkyblockExtension extends JavaPlugin {
-
+	
+	/**\
+	 * The instance of this plugin.
+	 * 
+	 * Private so that (at least I asume; this is from the bukkit tutorial) it is not overwritable.
+	 */
+	private static SkyblockExtension inst;
+	
+	/**\
+	 * Gets the instance of this plugin.
+	 * @return The instance of this plugin.
+	 */
+	public static SkyblockExtension inst() {
+		return inst;
+	}
+	
 }
