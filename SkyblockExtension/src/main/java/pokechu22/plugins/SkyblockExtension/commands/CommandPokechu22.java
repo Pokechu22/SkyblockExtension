3c91ed25b5645f9160fc1d908333310a9075af7a
Pokechu22 <Pokechu022@gmail.com>
2014-08-07 14:40:39 -0700

    Started /pokechu22 crashes Currently supports list.

--- SkyblockExtension/src/main/java/pokechu22/plugins/SkyblockExtension/commands/CommandPokechu22.java
+++ SkyblockExtension/src/main/java/pokechu22/plugins/SkyblockExtension/commands/CommandPokechu22.java
@@ -23,20 +23,63 @@ public class CommandPokechu22 {
 	 * @return True if the command syntax was correct, 
 	 *     false if you want the message in plugin.yml to be displayed. 
 	 */
-	public static boolean Run(CommandSender sender, Command cmd, String label, String[] args) throws Exception {
-		//TODO: This is debug code that should be removed.
-		if (args.length != 0) {
-			if (args[0].equalsIgnoreCase("catchtest")) {
-				throw new Exception("Test");
+	public static void Run(CommandSender sender, Command cmd, String label, String[] args) {
+		if (args.length == 0) {
+			sender.sendMessage("Usage: /" + label + " help");
+			return;
+		} else {
+			switch (args[0].toLowerCase()) {
+			case "crashtest": {
+				throw new RuntimeException("Test"); //TODO Test code.
 			}
-			if (args[0].equalsIgnoreCase("crashes")) {
-				ErrorHandler.listCrashes(sender, 0);
-				return true;
+			case "crashes": {
+				Crashes(sender,cmd,label,args);
+				return;
+			}
 			}
 		}
-		
-		
-		sender.sendMessage("Command sent sucessfully"); //TODO: Test.
-		return true;
+		sender.sendMessage("Usage: /" + label + " help");
+	}
+	
+	/**
+	 * Subcommand for /pokechu22 crashes.
+	 * @param sender
+	 * @param cmd
+	 * @param label
+	 * @param args
+	 * @Throws {@link Error} when args.length is 0, which should never happen.
+	 */
+	protected static void Crashes(CommandSender sender, Command cmd, String label, String[] args) {
+		if (args.length != 0) {
+			throw new Error("Args.length should NEVER be 0.");
+		}
+		if (args.length == 1) {
+			sender.sendMessage("Usage: /" + label + " crashes help");
+			return;
+		} else {
+			if (args[1].equalsIgnoreCase("list")) {
+				if (args.length == 2) {
+					ErrorHandler.listCrashes(sender, 0);
+					return;
+				} else if (args.length == 3){
+					int line;
+					try {
+						line = Integer.parseInt(args[2]);
+					} catch (NumberFormatException e) {
+						sender.sendMessage("§cFailed to parse line number (Got " +args[2] + ").");
+						sender.sendMessage("Usage: /" + label + " crashes help");
+						return;
+					}
+					ErrorHandler.listCrashes(sender, line);
+					return;
+				} else {
+					sender.sendMessage("§cError: Too many parameters.");
+					sender.sendMessage("Usage: /" + label + " crashes help");
+					return;
+				}
+			}
+		}
+		sender.sendMessage("Usage: /" + label + " crashes help");
+		return;
 	}
 }
