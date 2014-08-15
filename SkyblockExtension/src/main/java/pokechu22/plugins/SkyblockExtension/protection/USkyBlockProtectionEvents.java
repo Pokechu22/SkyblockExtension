fe0b1a0eda0f0a55d233d5e11f6c9e7f0c1749e3
Pokechu22 <Pokechu022@gmail.com>
2014-08-14 17:54:10 -0700

    More work on custom events. Removed the unused imports.

--- SkyblockExtension/src/main/java/pokechu22/plugins/SkyblockExtension/protection/USkyBlockProtectionEvents.java
+++ SkyblockExtension/src/main/java/pokechu22/plugins/SkyblockExtension/protection/USkyBlockProtectionEvents.java
@@ -1,14 +1,10 @@
 package pokechu22.plugins.SkyblockExtension.protection;
 
-import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
