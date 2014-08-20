fa88735179ee7de0b5be2be41859a27cc22d8d3f
Pokechu22 <Pokechu022@gmail.com>
2014-08-19 21:04:00 -0700

    More conventional method of serialization.

--- SkyblockExtension/src/main/java/pokechu22/plugins/SkyblockExtension/protection/IslandProtectionDataSet.java
+++ SkyblockExtension/src/main/java/pokechu22/plugins/SkyblockExtension/protection/IslandProtectionDataSet.java
@@ -1,10 +1,8 @@
 package pokechu22.plugins.SkyblockExtension.protection;
 
-import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.bukkit.Material;
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
@@ -194,13 +192,50 @@ public class IslandProtectionDataSet implements ConfigurationSerializable {
 	public Map<String, Object> serialize() {
 		HashMap<String, Object> map = new HashMap<String, Object>();
 		
-		for (Field f : this.getClass().getFields()) {
-			try {
-			map.put(f.getName(), f.get(map));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
+		map.put("canBuildAllBlocks", canBuildAllBlocks);
+		map.put("buildAllowedBlocks", buildAllowedBlocks);
+		map.put("buildBannedBlocks", buildBannedBlocks);
+		
+		map.put("canBreakAllBlocks", canBreakAllBlocks);
+		map.put("breakAllowedBlocks", breakAllowedBlocks);
+		map.put("breakBannedBlocks", breakBannedBlocks);
+		
+		map.put("canUseAllItems", canUseAllItems);
+		map.put("useAllowedBlocks", useAllowedBlocks);
+		map.put("useBannedBlocks", useBannedBlocks);
+		
+		map.put("canAttackAllEntities", canAttackAllEntities);
+		map.put("canAttackAnimals", canAttackAnimals);
+		map.put("canAttackHostile", canAttackHostile);
+		map.put("attackAllowedEntities", attackAllowedEntities);
+		map.put("attackBannedEntities", attackBannedEntities);
+		
+		map.put("canEat", canEat);
+		
+		map.put("canBreakAllHanging", canBreakAllHanging);
+		map.put("breakAllowedHangings", breakAllowedHangings);
+		map.put("breakBannedHangings", breakBannedHangings);
+		
+		map.put("canUseBeds", canUseBeds);
+		
+		map.put("canPVP", canPVP);
+		
+		map.put("canFillBuckets", canFillBuckets);
+		map.put("canEmptyBuckets", canEmptyBuckets);
+		
+		map.put("canShearSheep", canShearSheep);
+		
+		map.put("canUseAllEntities", canUseAllEntities);
+		map.put("useAllowedEntities", useAllowedEntities);
+		map.put("useBannedEntities", useBannedEntities);
+		
+		map.put("canDamageAllVehicles", canDamageAllVehicles);
+		map.put("damageAllowedVehicles", damageAllowedVehicles);
+		map.put("damageBannedVehicles", damageBannedVehicles);
+		
+		map.put("canEnterAllVehicles", canEnterAllVehicles);
+		map.put("enterAllowedVehicles", enterAllowedVehicles);
+		map.put("enterBannedVehicles", enterBannedVehicles);
 		
 		return map;
 	}
@@ -210,16 +245,52 @@ public class IslandProtectionDataSet implements ConfigurationSerializable {
 	 * @param map
 	 * @return
 	 */
+	@SuppressWarnings("unchecked")
 	public IslandProtectionDataSet(Map<String, Object> map) {
-		//Reflection shenanigans; I don't want to write everything out.
-		Set<String> keys = map.keySet();
-		for (String key : keys) {
-			try {
-				this.getClass().getField(key).set(this, map.get(key));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
+		this.canBuildAllBlocks = (boolean) map.get("canBuildAllBlocks");
+		this.buildAllowedBlocks = (List<Material>) map.get("buildAllowedBlocks");
+		this.buildBannedBlocks = (List<Material>) map.get("buildBannedBlocks");
+		
+		this.canBreakAllBlocks = (boolean) map.get("canBreakAllBlocks");
+		this.breakAllowedBlocks = (List<Material>) map.get("breakAllowedBlocks");
+		this.breakBannedBlocks = (List<Material>) map.get("breakBannedBlocks");
+		
+		this.canUseAllItems = (boolean) map.get("canUseAllItems");
+		this.useAllowedBlocks = (List<Material>) map.get("useAllowedBlocks");
+		this.useBannedBlocks = (List<Material>) map.get("useBannedBlocks");
+		
+		this.canAttackAllEntities = (boolean) map.get("canAttackAllEntities");
+		this.canAttackAnimals = (boolean) map.get("canAttackAnimals");
+		this.canAttackHostile = (boolean) map.get("canAttackHostile");
+		this.attackAllowedEntities = (List<EntityType>) map.get("attackAllowedEntities");
+		this.attackBannedEntities = (List<EntityType>) map.get("attackBannedEntities");
+		
+		this.canEat = (boolean) map.get("canEat");
+		
+		this.canBreakAllHanging = (boolean) map.get("canBreakAllHanging");
+		this.breakAllowedHangings = (List<HangingType>) map.get("breakAllowedHangings");
+		this.breakBannedHangings = (List<HangingType>) map.get("breakBannedHangings");
+		
+		this.canUseBeds = (boolean) map.get("canUseBeds");
+		
+		this.canPVP = (boolean) map.get("canPVP");
+		
+		this.canFillBuckets = (boolean) map.get("canFillBuckets");
+		this.canEmptyBuckets = (boolean) map.get("canEmptyBuckets");
+		
+		this.canShearSheep = (boolean) map.get("canShearSheep");
+		
+		this.canUseAllEntities = (boolean) map.get("canUseAllEntities");
+		this.useAllowedEntities = (List<EntityType>) map.get("useAllowedEntities");
+		this.useBannedEntities = (List<EntityType>) map.get("useBannedEntities");
+		
+		this.canDamageAllVehicles = (boolean) map.get("canDamageAllVehicles");
+		this.damageAllowedVehicles = (List<VehicleType>) map.get("damageAllowedVehicles");
+		this.damageBannedVehicles = (List<VehicleType>) map.get("damageBannedVehicles");
+		
+		this.canEnterAllVehicles = (boolean) map.get("canEnterAllVehicles");
+		this.enterAllowedVehicles = (List<VehicleType>) map.get("enterAllowedVehicles");
+		this.enterBannedVehicles = (List<VehicleType>) map.get("enterBannedVehicles");
 	}
 	
 	/**
