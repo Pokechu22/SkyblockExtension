3e479e80ac1582b8a7e7b03e7f0739eea9df3d3f
Pokechu22 <Pokechu022@gmail.com>
2014-08-25 15:41:28 -0700

    Implemented BooleanFlag.

--- SkyblockExtension/src/main/java/pokechu22/plugins/SkyblockExtension/protection/flags/BooleanFlag.java
+++ SkyblockExtension/src/main/java/pokechu22/plugins/SkyblockExtension/protection/flags/BooleanFlag.java
@@ -3,22 +3,58 @@ package pokechu22.plugins.SkyblockExtension.protection.flags;
 
 public class BooleanFlag extends IslandProtectionDataSetFlag {
 
+	protected boolean value;
+	
+	/**
+	 * Deserialization constructor.
+	 * @throws IllegalArgumentException when given invalid boolean.
+	 */
+	public BooleanFlag(String value) throws IllegalArgumentException {
+		if (value.equalsIgnoreCase("false")) {
+			this.value = false;
+		} else if (value.equalsIgnoreCase("true")) {
+			this.value = true;
+		} else {
+			throw new IllegalArgumentException("Invalid boolean type: " + 
+					value + ".");
+		}
+	}
+	
 	@Override
-	public String getType() {
-		// TODO Auto-generated method stub
-		return null;
+	public FlagType getType() {
+		return FlagType.BOOLEAN;
 	}
 
 	@Override
 	public String getSerializedValue() {
-		// TODO Auto-generated method stub
-		return null;
+		return Boolean.toString(value);
 	}
 
 	@Override
 	public String getDispayValue() {
-		// TODO Auto-generated method stub
-		return null;
+		return Boolean.toString(value);
+	}
+
+	@Override
+	public String setValue(String value) {
+		if (value.equalsIgnoreCase("false")) {
+			this.value = false;
+		} else if (value.equalsIgnoreCase("true")) {
+			this.value = true;
+		} else {
+			return "§c\"" + value + "\" is not a valid boolean value!";
+		}
+		return "§aValue set sucessfully.";
+	}
+
+	@Override
+	public boolean canAddToValue() {
+		return false;
+	}
+
+	@Override
+	public String addToValue(String addition) {
+		return "§cBooleans cannot be added to!";
 	}
 	
 }
