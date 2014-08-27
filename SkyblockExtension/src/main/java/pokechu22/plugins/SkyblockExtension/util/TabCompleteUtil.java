a0273750df27bbf47a8d9e7ca12ea4011e8c52e3
Pokechu22 <Pokechu022@gmail.com>
2014-08-26 17:15:59 -0700

    WIP code...  Going to remove this and do it ... again.

--- SkyblockExtension/src/main/java/pokechu22/plugins/SkyblockExtension/util/TabCompleteUtil.java
+++ SkyblockExtension/src/main/java/pokechu22/plugins/SkyblockExtension/util/TabCompleteUtil.java
@@ -3,6 +3,7 @@ package pokechu22.plugins.SkyblockExtension.util;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Tab-completion utilities, such as limiting to the argument.
@@ -18,10 +19,11 @@ public class TabCompleteUtil {
 	 * @param starting
 	 * @return
 	 */
-	public static List<String> TabLimit(List<String> list, String starting) {
+	public static List<String> TabLimit(List<String> list, String starting){
 		ArrayList<String> returned = new ArrayList<String>();
 		for (String s : list) {
-			if (s.startsWith(starting)) {
+			if (s.toLowerCase(Locale.ENGLISH).startsWith(starting
+					.toLowerCase(Locale.ENGLISH))) {
 				returned.add(s);
 			}
 		}
@@ -36,14 +38,75 @@ public class TabCompleteUtil {
 	 * @return
 	 */
 	public static List<String> TabLimit(String[] values, String starting) {
-		List<String> list = Arrays.asList(values);
+		return TabLimit(Arrays.asList(values), starting);
+	}
+	
+	public static List<String> TabLimit(String[] partialValues, 
+			String[] options) {
+		List<String> returned = new ArrayList<String>();
 		
-		ArrayList<String> returned = new ArrayList<String>();
-		for (String s : list) {
-			if (s.startsWith(starting)) {
-				returned.add(s);
+		if (partialValues.length == 0) {
+			returned.add("[");
+			return returned;
+		}
+		
+		//First letter to be used.
+		int firstChar = 0;
+		
+		if (partialValues.length == 1) {
+			//Starting char.
+			if (partialValues[0].length() == 0) {
+				returned.add("[");
+				return returned;				
+			}
+			if (!partialValues[0].startsWith("[")) {
+				partialValues[0] = "[" + partialValues[0];
+				firstChar = 1;
 			}
 		}
+		
+		//Index in the array.
+		int i = partialValues.length - 1;
+		
+		String subValue = (partialValues[i]).substring(
+				(partialValues[i].contains(",") ? 
+						partialValues[i].lastIndexOf(",") : firstChar), 
+						partialValues[i].length());
+		
+		for (String option : options) {
+			//If it is an EXACT match
+			if (option.equalsIgnoreCase(subValue)) {
+				returned.add(partialValues[i] + ", ");
+				return returned;
+			}
+			//If it starts.
+			if (option.toUpperCase(Locale.ENGLISH)
+					.startsWith(subValue.toUpperCase(Locale.ENGLISH))) {
+				//Get the new section and add it.
+				String origional = (partialValues[i]).substring(firstChar,
+						(partialValues[i].contains(",") ? 
+								partialValues[i].lastIndexOf(",") : 
+									partialValues[i].length()));
+				String addition = partialValues[i].replace(subValue, "");
+				returned.add(origional + addition);
+			}
+		}
+		
 		return returned;
+	}
+	
+	public static List<String> TabLimit(String[] partialValues, 
+			List<String> options) {
+		return TabLimit(partialValues, 
+				options.toArray(new String[options.size()]));
+	}
+	
+	public static List<String> TabLimit(String[] partialValues, 
+			Enum<?>[] options) {
+		String[] optionsList = new String[options.length];
+		for (int i = 0; i < options.length; i++) {
+			optionsList[i] = options[i].toString();
+		}
+		return TabLimit(partialValues, optionsList);
 	}
 }
