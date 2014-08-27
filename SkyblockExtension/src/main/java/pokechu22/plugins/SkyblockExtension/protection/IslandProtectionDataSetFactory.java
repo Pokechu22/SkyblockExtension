0e24b8559af60bacd29c2c3d75503b6d4a3ce503
Pokechu22 <Pokechu022@gmail.com>
2014-08-26 22:39:58 -0700

    Actually, it would make more sense to only have one config.

--- SkyblockExtension/src/main/java/pokechu22/plugins/SkyblockExtension/protection/IslandProtectionDataSetFactory.java
+++ SkyblockExtension/src/main/java/pokechu22/plugins/SkyblockExtension/protection/IslandProtectionDataSetFactory.java
@@ -15,69 +15,24 @@ import pokechu22.plugins.SkyblockExtension.SkyblockExtension;
  */
 public class IslandProtectionDataSetFactory {
 	
-	private FileConfiguration defaultOwnerConfig = null;
-	private File defaultOwnerConfigFile = null;
-	
-	private FileConfiguration defaultMemberConfig = null;
-	private File defaultMemberConfigFile = null;
-	
-	private FileConfiguration defaultGuestConfig = null;
-	private File defaultGuestConfigFile = null;
-	
-	private FileConfiguration defaultNonmemberConfig = null;
-	private File defaultNonmemberConfigFile = null;
+	private FileConfiguration defaultProtectionConfig = null;
+	private File defaultProtectionConfigFile = null;
 
 	/**
 	 * Saves the default configs.
 	 */
 	public void saveDefaultConfigs() {
 		//Owner
-		if (defaultOwnerConfigFile == null) {
-			defaultOwnerConfigFile = new File(SkyblockExtension.inst()
-					.getDataFolder(), "defaultownerconfig.yml");
+		if (defaultProtectionConfigFile == null) {
+			defaultProtectionConfigFile = new File(SkyblockExtension.inst()
+					.getDataFolder(), "default_protection.yml");
 		}
-		if (!defaultOwnerConfigFile.exists()) {
-			SkyblockExtension.inst().saveResource("defaultownerconfig.yml"
+		if (!defaultProtectionConfigFile.exists()) {
+			SkyblockExtension.inst().saveResource("default_protection.yml"
 					, false);
 		}
-		defaultOwnerConfig = YamlConfiguration
-				.loadConfiguration(defaultOwnerConfigFile);
-		
-		//Member
-		if (defaultMemberConfigFile == null) {
-			defaultMemberConfigFile = new File(SkyblockExtension.inst()
-					.getDataFolder(), "defaultmemberconfig.yml");
-		}
-		if (!defaultMemberConfigFile.exists()) {
-			SkyblockExtension.inst().saveResource("defaultmemberconfig.yml"
-					, false);
-		}
-		defaultMemberConfig = YamlConfiguration
-				.loadConfiguration(defaultMemberConfigFile);
-
-		//Guest
-		if (defaultGuestConfigFile == null) {
-			defaultGuestConfigFile = new File(SkyblockExtension.inst()
-					.getDataFolder(), "defaultguestconfig.yml");
-		}
-		if (!defaultGuestConfigFile.exists()) {
-			SkyblockExtension.inst().saveResource("defaultguestconfig.yml"
-					, false);
-		}
-		defaultGuestConfig = YamlConfiguration
-				.loadConfiguration(defaultGuestConfigFile);
-
-		//Nonmember
-		if (defaultNonmemberConfigFile == null) {
-			defaultNonmemberConfigFile = new File(SkyblockExtension.inst()
-					.getDataFolder(), "defaultnonmemberconfig.yml");
-		}
-		if (!defaultNonmemberConfigFile.exists()) {
-			SkyblockExtension.inst()
-				.saveResource("defaultnonmemberconfig.yml", false);
-		}
-		defaultNonmemberConfig = YamlConfiguration
-				.loadConfiguration(defaultNonmemberConfigFile);
+		defaultProtectionConfig = YamlConfiguration
+				.loadConfiguration(defaultProtectionConfigFile);
 
 	}
 }
