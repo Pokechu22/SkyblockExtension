2768954013d7c4438c28557f14201bf5de0907e6
Pokechu22 <Pokechu022@gmail.com>
2014-09-01 13:27:26 -0700

    Importing mojang NBT code This code is the same as that found in https://mojang.com/2012/02/new-minecraft-map-format-anvil/ except that the package was changed.

---
+++ SkyblockExtension/src/main/java/pokechu22/plugins/SkyblockExtension/util/nbt/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * NBT Code, copied from Mojang code found at 
+ * https://mojang.com/2012/02/new-minecraft-map-format-anvil/
+ * 
+ * Note that this code was slightly modified from the original version
+ * due to needing to change the package.
+ * 
+ * @author Mojang
+ */
+package pokechu22.plugins.SkyblockExtension.util.nbt;
