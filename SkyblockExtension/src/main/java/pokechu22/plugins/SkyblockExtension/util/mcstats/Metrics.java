493e946c83a9962c0748a4a322ad998402a0201c
Pokechu22 <pokechu022@gmail.com>
2014-09-17 06:56:24 -0700

    Fixed package declaration.
    #3

--- SkyblockExtension/src/main/java/pokechu22/plugins/SkyblockExtension/util/mcstats/Metrics.java
+++ SkyblockExtension/src/main/java/pokechu22/plugins/SkyblockExtension/util/mcstats/Metrics.java
@@ -25,7 +25,7 @@
  * authors and contributors and should not be interpreted as representing official policies,
  * either expressed or implied, of anybody else.
  */
-package org.mcstats;
+package pokechu22.plugins.SkyblockExtension.util.mcstats;
 
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.InvalidConfigurationException;
