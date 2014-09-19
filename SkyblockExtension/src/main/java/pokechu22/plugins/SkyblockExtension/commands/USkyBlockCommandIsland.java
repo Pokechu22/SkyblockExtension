5292c968c8a731e1968ce471d3ccaf8a2de5ca9c
Pokechu22 <pokechu022@gmail.com>
2014-09-19 11:03:10 -0700

    Some work on run, using the (very terrible) existing decompilation.
    (WTF is this.val$playerx?)
    
    Issue #4.

--- SkyblockExtension/src/main/java/pokechu22/plugins/SkyblockExtension/commands/USkyBlockCommandIsland.java
+++ SkyblockExtension/src/main/java/pokechu22/plugins/SkyblockExtension/commands/USkyBlockCommandIsland.java
@@ -12,6 +12,9 @@ import java.util.Map;
 import java.util.TreeMap;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
@@ -24,6 +27,7 @@ import pokechu22.plugins.SkyblockExtension.protection.IslandInfo;
 import pokechu22.plugins.SkyblockExtension.util.IslandUtils;
 import us.talabrek.ultimateskyblock.IslandCommand;
 import us.talabrek.ultimateskyblock.PlayerInfo;
+import us.talabrek.ultimateskyblock.Settings;
 import us.talabrek.ultimateskyblock.uSkyBlock;
 
 /**
@@ -100,13 +104,157 @@ public class USkyBlockCommandIsland extends IslandCommand implements TabComplete
 		return;
 	}
 	
+	/**
+	 * Calculates island level.
+	 * 
+	 * @param player
+	 * @param islandPlayer
+	 * @return
+	 */
+	@Override
+	public boolean getIslandLevel(Player player, String islandPlayer) {
+		if (!useCustomBlockValues) {
+			return super.getIslandLevel(player, islandPlayer);
+		} else {
+			if ((!uSkyBlock.getInstance().hasIsland(islandPlayer)) && 
+					(!uSkyBlock.getInstance().hasParty(islandPlayer))) {
+				player.sendMessage(ChatColor.RED + 
+						"That player is invalid or does not have an island!");
+				this.allowInfo = true;
+				return false;
+			}
+			SkyblockExtension.inst().getServer().getScheduler()
+					.runTaskAsynchronously(SkyblockExtension.inst(),
+							new AsyncIslandLevelCalculator(player, 
+									islandPlayer));
+			return true;
+		}
+	}
+	
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
  * END OF USKYBLOCK CODE! NONE OF THIS IS PART OF THE ORIGIONAL COMMAND. *
 \* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 	
+	/**
+	 * Asynchronously calculates the island level.  
+	 *
+	 * @author Pokechu22
+	 *
+	 */
+	protected static class AsyncIslandLevelCalculator implements Runnable {
+
+		private Player player;
+		private String islandPlayer;
+		
+		public AsyncIslandLevelCalculator(Player player, String islandPlayer) {
+			this.player = player;
+			this.islandPlayer = islandPlayer;
+		}
+		
+		@Override
+		public void run() {
+			/* 1399 */           System.out.print("Calculating island level in async thread");
+			/*      */           try {
+			/* 1401 */             String playerName = player.getName();
+			/*      */             Location l;
+			/* 1409 */             if (((PlayerInfo)uSkyBlock.getInstance().getActivePlayers().get(playerName)).getHasParty())
+			/*      */             {
+			/* 1411 */               l = ((PlayerInfo)uSkyBlock.getInstance().getActivePlayers().get(playerName)).getPartyIslandLocation();
+			/*      */             }
+			/*      */             else
+			/* 1414 */               l = ((PlayerInfo)uSkyBlock.getInstance().getActivePlayers().get(playerName)).getIslandLocation();
+			/* 1415 */             int blockcount = 0;
+			/* 1416 */             if (playerName.equalsIgnoreCase(islandPlayer))
+			/*      */             {
+			/* 1418 */               int cobblecount = 0;
+			/* 1419 */               int px = l.getBlockX();
+			/* 1420 */               int py = l.getBlockY();
+			/* 1421 */               int pz = l.getBlockZ();
+			/* 1422 */               for (int x = -50; x <= 50; x++) {
+			/* 1423 */                 for (int y = Settings.island_height * -1; y <= 255 - Settings.island_height; y++) {
+			/* 1424 */                   for (int z = -50; z <= 50; z++)
+			/*      */                   {
+			/* 1426 */                     Block b = new Location(l.getWorld(), px + x, py + y, pz + z).getBlock();
+			/* 1427 */                     if (b.getTypeId() == 57)
+			/* 1428 */                       blockcount += 300;
+			/* 1429 */                     if ((b.getTypeId() == 41) || (b.getTypeId() == 116) || (b.getTypeId() == 122))
+			/* 1430 */                       blockcount += 150;
+			/* 1431 */                     if ((b.getTypeId() == 49) || (b.getTypeId() == 42))
+			/* 1432 */                       blockcount += 10;
+			/* 1433 */                     if ((b.getTypeId() == 47) || (b.getTypeId() == 84))
+			/* 1434 */                       blockcount += 5;
+			/* 1435 */                     if ((b.getTypeId() == 79) || (b.getTypeId() == 82) || (b.getTypeId() == 112) || (b.getTypeId() == 2) || (b.getTypeId() == 110))
+			/* 1436 */                       blockcount += 3;
+			/* 1437 */                     if ((b.getTypeId() == 98) || (b.getTypeId() == 45) || (b.getTypeId() == 35) || (b.getTypeId() == 24) || 
+			/* 1438 */                       (b.getTypeId() == 121) || (b.getTypeId() == 108) || (b.getTypeId() == 109) || (b.getTypeId() == 43) || 
+			/* 1439 */                       (b.getTypeId() == 20))
+			/* 1440 */                       blockcount += 2;
+			/* 1441 */                     if (((b.getTypeId() != 0) && (b.getTypeId() != 8) && (b.getTypeId() != 9) && (b.getTypeId() != 10) && (b.getTypeId() != 11) && (b.getTypeId() != 4)) || ((b.getTypeId() == 4) && (cobblecount < 10000)))
+			/*      */                     {
+			/* 1443 */                       blockcount++;
+			/* 1444 */                       if (b.getTypeId() == 4)
+			/*      */                       {
+			/* 1446 */                         cobblecount++;
+			/*      */                       }
+			/*      */                     }
+			/*      */                   }
+			/*      */                 }
+			/*      */               }
+			/*      */             }
+			/*      */ 
+			/* 1454 */             if (playerName.equalsIgnoreCase(islandPlayer))
+			/*      */             {
+			/* 1456 */               ((PlayerInfo)uSkyBlock.getInstance().getActivePlayers().get(playerName)).setIslandLevel(blockcount / 100);
+			/*      */             }
+			/*      */           } catch (Exception e) {
+			/* 1459 */             System.out.print("Error while calculating Island Level: " + e);
+			/*      */           }
+			/* 1462 */           System.out.print("Finished async info thread");
+			/*      */ 
+//			/* 1465 */           uSkyBlock.getInstance().getServer().getScheduler().scheduleSyncDelayedTask(uSkyBlock.getInstance(), new Runnable()
+//			/*      */           {
+//			/*      */             public void run()
+//			/*      */             {
+//			/* 1471 */               System.out.print("Back to sync thread for info");
+//			/* 1472 */               if (Bukkit.getPlayer(this) != null)
+//			/*      */               {
+//			/* 1474 */                 Bukkit.getPlayer(this.val$playerx).sendMessage(ChatColor.YELLOW + "Information about " + this.val$islandPlayerx + "'s Island:");
+//			/* 1475 */                 if (this.val$playerx.equalsIgnoreCase(this.val$islandPlayerx))
+//			/*      */                 {
+//			/* 1477 */                   Bukkit.getPlayer(this.val$playerx).sendMessage(ChatColor.GREEN + "Island level is " + ChatColor.WHITE + ((PlayerInfo)uSkyBlock.getInstance().getActivePlayers().get(this.val$playerx)).getIslandLevel());
+//			/*      */                 }
+//			/*      */                 else {
+//			/* 1480 */                   PlayerInfo pi = uSkyBlock.getInstance().readPlayerFile(this.val$islandPlayerx);
+//			/* 1481 */                   if (pi != null)
+//			/* 1482 */                     Bukkit.getPlayer(this.val$playerx).sendMessage(ChatColor.GREEN + "Island level is " + ChatColor.WHITE + pi.getIslandLevel());
+//			/*      */                   else
+//			/* 1484 */                     Bukkit.getPlayer(this.val$playerx).sendMessage(ChatColor.RED + "Error: Invalid Player");
+//			/*      */                 }
+//			/*      */               }
+//			/* 1487 */               System.out.print("Finished with sync thread for info");
+//			/*      */             }
+//			/*      */           }
+//			/*      */           , 0L);
+			/*      */         
+		}
+		
+	}
+	
+	/**
+	 * Enable the enhanced help2 system?
+	 */
 	public static boolean enableHelp2 = true;
 	
+	/**
+	 * Allow uSkyBlock to be case insensitive? 
+	 * (Potential source of bugs!)
+	 */
 	public static boolean allowIgnoreCase = true;
+	
+	/**
+	 * Use custom island block value calculator?
+	 */
+	public static boolean useCustomBlockValues = true;
 	
 	/**
 	 * Registers all hooks on this command, overriding uSkyBlock code.
