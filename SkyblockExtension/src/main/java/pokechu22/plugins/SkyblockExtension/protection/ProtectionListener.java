2a990088530ef8aba2c7a0c2b2a0ef55d2c6bb71
Pokechu22 <pokechu022@gmail.com>
2014-09-23 11:15:47 -0700

    Implemented entityattacking check.

--- SkyblockExtension/src/main/java/pokechu22/plugins/SkyblockExtension/protection/ProtectionListener.java
+++ SkyblockExtension/src/main/java/pokechu22/plugins/SkyblockExtension/protection/ProtectionListener.java
@@ -3,9 +3,13 @@ package pokechu22.plugins.SkyblockExtension.protection;
 import static pokechu22.plugins.SkyblockExtension.util.IslandUtils.*;
 
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+
+import pokechu22.plugins.SkyblockExtension.util.EntityPasivityUtil;
 
 public class ProtectionListener {
 	/**
@@ -38,6 +42,66 @@ public class ProtectionListener {
 					return;
 				}
 			}
+		}
+	}
+	
+	public void onEntityAttackEntity(EntityDamageByEntityEvent e) {
+		//Do some stuff to check if the attacker is a player.
+		Player attacker = null;
+		
+		if (e.getDamager() instanceof Player) {
+			attacker = (Player) e.getDamager();
+		} else if (e.getDamager() instanceof Projectile) {
+			//Changed from uSkyBlock:
+			//now uses projectile rather than arrow.
+			Projectile p = (Projectile) e.getDamager();
+			if (p.getShooter() instanceof Player) {
+				attacker = (Player) p.getShooter();
+			}
+		}
+		
+		if (attacker == null) {
+			//If the attacker wasn't a player.
+			return;
+		}
+		
+		if (hasModOverride(attacker)) {
+			return;
+		}
+		
+		IslandProtectionDataSet set = getDataSetFor(attacker, 
+				e.getEntity().getLocation());
+		
+		if (set.attackBannedEntities.getValue().contains(e.getEntity().getType())) {
+			e.setCancelled(true);
+			attacker.sendMessage("§cYou aren't allowed to do that in this area!");
+			return;
+		} else {
+			//Deviating from standard code here.  Animals/hostiles are tested.
+			if (set.canAttackAllEntities.getValue()) {
+				return;
+			}
+			
+			if (set.canAttackAnimals.getValue()) {
+				if (EntityPasivityUtil.isPassive(e.getEntityType())) {
+					return;
+				}
+			}
+			if (set.canAttackHostile.getValue()) {
+				if (EntityPasivityUtil.isHostile(e.getEntityType())) {
+					return;
+				}
+			}
+			
+			//else {
+			if (set.attackAllowedEntities.getValue().contains(e.getEntity().getType())) {
+				return;
+			} else {
+				e.setCancelled(true);
+				attacker.sendMessage("§cYou aren't allowed to do that in this area!");
+				return;
+			}
+			//}
 		}
 	}
 	
