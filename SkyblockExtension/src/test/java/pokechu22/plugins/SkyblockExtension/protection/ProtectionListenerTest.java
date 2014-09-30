8ee251744b6feff664c0dc1a48248a0822e2711e
Pokechu22 <pokechu022@gmail.com>
2014-09-30 14:25:00 -0700

    Started on a test for the ProtectionListener.

---
+++ SkyblockExtension/src/test/java/pokechu22/plugins/SkyblockExtension/protection/ProtectionListenerTest.java
@@ -0,0 +1,21 @@
+package pokechu22.plugins.SkyblockExtension.protection;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.junit.Test;
+
+/**
+ * Tests the {@link ProtectionListener}.
+ * 
+ * @author Pokechu22
+ *
+ */
+public class ProtectionListenerTest {
+	@Test
+	public void onEntityInteractTest() {
+		//TODO
+		//PlayerInteractEntityEvent event = new PlayerInteractEntityEvent(null, null);
+	}
+}
