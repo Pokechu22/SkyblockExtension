576bc9fca09deebf9c5349429a5f250b31083de8
Pokechu22 <pokechu022@gmail.com>
2014-10-01 12:48:21 -0700

    More work on this thing's formating.

--- SkyblockExtension/src/main/java/pokechu22/plugins/SkyblockExtension/protection/flags/MaterialToMatierialListMapFlag.java
+++ SkyblockExtension/src/main/java/pokechu22/plugins/SkyblockExtension/protection/flags/MaterialToMatierialListMapFlag.java
@@ -3,19 +3,65 @@ package pokechu22.plugins.SkyblockExtension.protection.flags;
 import static pokechu22.plugins.SkyblockExtension.util.TabCompleteUtil.TabLimit;
 
 import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.EnumSet;
 import java.util.List;
 
-import org.bukkit.entity.EntityType;
+import org.bukkit.Material;
 
+/**
+ * Flag mapping materials (EG in hand) to a list of materials 
+ * (eg blocks that can interacted with)
+ * 
+ * TODO: This is a terrible name.
+ * 
+ * @author Pokechu22
+ *
+ */
 public class MaterialToMatierialListMapFlag extends IslandProtectionDataSetFlag {
 
-	protected Object value;
+	/**
+	 * Data for the individual values, eg things that can be placed on.
+	 * 
+	 * It's a []-based list, but it has the following special conditions:
+	 * [*] = all values allowed.  [*, -DIRT] bans placement of dirt only.
+	 * If it uses *, it is invalid to have any non-minus values; without
+	 * the * it is illegal to have any minus values.
+	 * 
+	 * TODO: This is a worse name :(
+	 * 
+	 * @author Pokechu22
+	 *
+	 */
+	static class SecondHalfData {
+		/**
+		 * Gets a serialized reperenstentation.
+		 * 
+		 * @return
+		 */
+		public String serialize() {
+			return null; //TODO
+		}
+		
+		/**
+		 * Loads the values from the serialized version into this.
+		 * 
+		 * @param serialized
+		 */
+		public void deserialize(String serialized) {
+			//TODO
+		}
+	}
+	
+	protected EnumMap<Material, EnumSet<Material>> value;
 	
 	/**
 	 * Deserialization.
 	 */
 	public MaterialToMatierialListMapFlag(String serialized)
 			throws IllegalArgumentException {
+		value = new EnumMap<Material, EnumSet<Material>>(Material.class);
+		
 		String result = this.setValue(serialized);
 		if (result.startsWith("§a")) {
 			return; //Valid value.
