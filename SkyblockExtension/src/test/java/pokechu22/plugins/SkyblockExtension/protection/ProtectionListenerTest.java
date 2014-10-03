d5178a5ca1c7d348ba21a9d093f2475408e524c8
Pokechu22 <pokechu022@gmail.com>
2014-10-03 11:59:36 -0700

    Tried to write a test.  But it won't work due to stupid setups.
    I should move listener stuff to the dataset.

--- SkyblockExtension/src/test/java/pokechu22/plugins/SkyblockExtension/protection/ProtectionListenerTest.java
+++ SkyblockExtension/src/test/java/pokechu22/plugins/SkyblockExtension/protection/ProtectionListenerTest.java
@@ -2,7 +2,11 @@ package pokechu22.plugins.SkyblockExtension.protection;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+import static org.mockito.Mockito.*;
 
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.junit.Test;
 
@@ -13,9 +17,57 @@ import org.junit.Test;
  *
  */
 public class ProtectionListenerTest {
+	/**
+	 * Gets a mock player at the specified location.
+	 * Also specifies permissions and stuff like that.
+	 * 
+	 * @param location
+	 * @return
+	 */
+	private Player getMockPlayer(Location location) {
+		Player mockPlayer = mock(Player.class);
+		when(mockPlayer.getLocation()).thenReturn(location);
+		
+		//TODO this is due to saved files.
+		//Won't work right on other computers.
+		when(mockPlayer.getName()).thenReturn("pokechu22"); 
+		when(mockPlayer.hasPermission("usb.mod.bypassprotection")).thenReturn(false);
+		
+		return mockPlayer;
+	}
+	
+	/**
+	 * Gets a mock entity at the specified location.
+	 * 
+	 * @param location
+	 * @return
+	 */
+	private Entity getMockEntity(Location location) {
+		Entity mockEntity = mock(Entity.class);
+		when(mockEntity.getLocation()).thenReturn(location);
+		
+		return mockEntity;
+	}
+	
+	/**
+	 * Tests the 
+	 * {@link ProtectionListener#onEntityInteract(PlayerInteractEntityEvent)}
+	 * method.
+	 */
 	@Test
 	public void onEntityInteractTest() {
-		//TODO
-		//PlayerInteractEntityEvent event = new PlayerInteractEntityEvent(null, null);
+		Player mockPlayer = getMockPlayer(new Location(null, 0, 0, 100));
+		
+		Entity mockEntity = getMockEntity(new Location(null, 0, 0, 0));
+		
+		ProtectionListener l = new ProtectionListener();
+		
+		PlayerInteractEntityEvent event = new PlayerInteractEntityEvent(
+				mockPlayer, mockEntity);
+		event.setCancelled(false);
+		
+		l.onEntityInteract(event);
+		
+		assertThat(event.isCancelled(), is(true));
 	}
 }
