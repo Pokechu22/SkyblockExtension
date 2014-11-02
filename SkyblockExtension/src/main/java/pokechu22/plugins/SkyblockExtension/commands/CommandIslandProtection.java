cd901acbeaae05c00cb02d0ec3fab4ec7b94ba3b
Pokechu22 <Pokechu022@gmail.com>
2014-11-01 18:39:17 -0700

    Trying to improve the command.

--- SkyblockExtension/src/main/java/pokechu22/plugins/SkyblockExtension/commands/CommandIslandProtection.java
+++ SkyblockExtension/src/main/java/pokechu22/plugins/SkyblockExtension/commands/CommandIslandProtection.java
@@ -15,28 +15,36 @@ import pokechu22.plugins.SkyblockExtension.protection.IslandProtectionDataSet;
 import pokechu22.plugins.SkyblockExtension.protection.MembershipTier;
 import pokechu22.plugins.SkyblockExtension.protection.flags.IslandProtectionDataSetFlag;
 
+/**
+ * Island-protection configuration command.  
+ * 
+ * Format: 
+ * /isprot [name|location] [tier] [flag] [action] [values...]
+ * 
+ * @author Pokechu22
+ *
+ */
 public class CommandIslandProtection {
-	/**
-	 * Subcommands, belonging to args[0].
-	 */
-	private static final String[] subCommands = new String[] {
-		"get", //Shows a flag's value.
-		"add", //Adds to a flag's value.
-		"add-f", //Forcibly adds to a flag's value.
-		"set" //Sets a flag's value.
-	};
-	
 	/**
 	 * Levels of membership.
 	 */
-	private static final String[] membershipTiers;
+	private static final List<String> tiers;
+	
+	/**
+	 * Different flags avaliable.
+	 */
+	private static final List<String> flags;
 	
 	static {
-		MembershipTier[] values = MembershipTier.values();
-		membershipTiers = new String[values.length];
-		for (int i = 0; i < values.length; i++) {
-			membershipTiers[i] = values[i].name();
+		tiers = new ArrayList<>();
+		for (MembershipTier tier : MembershipTier.values()) {
+			tiers.add(tier.name().toLowerCase(Locale.ENGLISH));
 		}
+	}
+	
+	static {
+		flags = new ArrayList<>();
+		flags.addAll(IslandProtectionDataSetFlag.flagTypes.keySet());
 	}
 	
 	/**
@@ -50,37 +58,55 @@ public class CommandIslandProtection {
 	 */
 	public static List<String> onTabComplete(CommandSender sender, Command cmd, String label, String args[]) 
 	{
+		if (args.length == 0) {
+			//Produce tiers from scratch
+			return tiers;
+		}
 		if (args.length == 1) {
-			return TabLimit(Arrays.asList(subCommands), args[0]);
+			//Produce tiers based off of args.
+			return TabLimit(tiers, args[0]);
 		}
 		if (args.length == 2) {
-			if (Arrays.asList(subCommands).contains(args[0].toLowerCase(Locale.ENGLISH))) {
-				return TabLimit(Arrays.asList(membershipTiers), args[1]);
-			}
-		}
-		if (args.length == 3) {
-			ArrayList<String> ret = new ArrayList<>();
-			for (Map.Entry<String, IslandProtectionDataSetFlag.FlagType> e : 
-					IslandProtectionDataSetFlag.flagTypes.entrySet()) {
-				if (e.getValue().canPreformAction(args[1].toLowerCase(Locale.ENGLISH))) {
-					ret.add(e.getKey());
-				}
-			}
-			return TabLimit(ret, args[2]);
-		}
-		
-		if (args.length >= 4) {
-			MembershipTier parsedTier = MembershipTier.matchTier(args[1]); 
-			
-			if (parsedTier == null) {
-				//Unlike with run, we don't want to send messages.
+			//Validate tiers.
+			if (!tiers.contains(args[0])) {
 				return new ArrayList<String>();
 			}
 			
-			IslandProtectionDataSet relevantDataSet = tieredValues.get(parsedTier);
+			//Produce flag name.
+			return TabLimit(flags, args[1]);
+		}
+		if (args.length == 3) {
+			//Validate tiers.
+			if (!tiers.contains(args[0])) {
+				return new ArrayList<String>();
+			}
 			
-			return relevantDataSet.tabComplete
-					(args[2], args[0], Arrays.copyOfRange(args, 3, args.length));
+			//Validate flag.
+			if (!IslandProtectionDataSetFlag.flagTypes.entrySet().contains(args[1])) {
+				return new ArrayList<String>();
+			}
+			
+			return TabLimit(IslandProtectionDataSetFlag.flagTypes.get(args[1]).getAllowedActions(), args[2]);
+		}
+		
+		if (args.length >= 4) {
+			//Validate tiers.
+			if (!tiers.contains(args[0])) {
+				return new ArrayList<String>();
+			}
+			
+			//Validate flag.
+			if (!IslandProtectionDataSetFlag.flagTypes.entrySet().contains(args[1])) {
+				return new ArrayList<String>();
+			}
+			
+			//Validate action.
+			if (!IslandProtectionDataSetFlag.flagTypes.get(args[1]).canPreformAction(args[3])) {
+				return new ArrayList<String>();
+			}
+		
+			//TODO: Get the location.
+			return new ArrayList<String>();
 		}
 
 		//Basically, return nothing, rather than null which gives all online players.
@@ -90,44 +116,13 @@ public class CommandIslandProtection {
 	/**
 	 * Runs the command.
 	 * 
-	 * TODO: Make better (EG Error messages).
-	 * 
 	 * @param sender
 	 * @param cmd
 	 * @param label
 	 * @param args
 	 */
 	public static void Run(CommandSender sender, Command cmd, String label, String args[]) {
-		/*if (args.length < 3) {
-			return;
-			//TODO error message.
-		}*/
-		
-		MembershipTier parsedTier = MembershipTier.matchTier(args[1]); 
-		
-		if (parsedTier == null) {
-			sender.sendMessage("§cInvlaid tier"); //TODO better message
-			return;
-		}
-		
-		IslandProtectionDataSet relevantDataSet = tieredValues.get(parsedTier.name());
-		
-		if (args.length >= 3) {
-			String longParam = "";
-			for (int i = 3; i < args.length; i++) {
-				longParam += args[i];
-				longParam += " ";
-			}
-			longParam = longParam.trim();
-			
-			relevantDataSet.preformActionOnFlag(args[2], args[0], 
-					Arrays.copyOfRange(args, 3, args.length));
-		}
-		if (args.length == 2) {
-			if (args[0].equalsIgnoreCase("get")) {
-				sender.sendMessage(relevantDataSet.getAllValuesForChat());
-			}
-		}
+		//TODO
 	}
 	
 	/**
