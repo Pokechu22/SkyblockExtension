e6c5b4dcfe9c06565fb20288db7dfd85b1421d52
Pokechu22 <Pokechu022@gmail.com>
2015-01-11 17:18:56 -0800

    Cleaned up metrics code and removed error handler count code.
    Error handler counts are useless in practice unless I know the exact
    data.

--- SkyblockExtension/src/main/java/pokechu22/plugins/SkyblockExtension/util/mcstats/MetricsHandler.java
+++ SkyblockExtension/src/main/java/pokechu22/plugins/SkyblockExtension/util/mcstats/MetricsHandler.java
@@ -1,15 +1,11 @@
 package pokechu22.plugins.SkyblockExtension.util.mcstats;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.bukkit.Bukkit;
 
 import pokechu22.plugins.SkyblockExtension.SkyblockExtension;
 import pokechu22.plugins.SkyblockExtension.WitherWarner;
-import pokechu22.plugins.SkyblockExtension.errorhandling.CrashReport;
-import pokechu22.plugins.SkyblockExtension.errorhandling.ErrorHandler;
 import pokechu22.plugins.SkyblockExtension.util.PlayerPrintStream;
 import pokechu22.plugins.SkyblockExtension.util.mcstats.Metrics.Graph;
 import pokechu22.plugins.SkyblockExtension.util.mcstats.Metrics.Plotter;
@@ -30,84 +26,20 @@ public class MetricsHandler {
 		return metrics;
 	}
 
-	static class BooleanPlotter extends Plotter {
-		public final boolean value;
-
-		/**
-		 * Constructs a BooleanPlotter with that name and value.
-		 * @param name
-		 */
-		public BooleanPlotter(String name, boolean value) {
-			super (name);
-			this.value = value;
-		}
-
-		@Override
-		public int getValue() {
-			return value ? 0 : 1;
-		}
-	}
-	
-	static class IntPlotter extends Plotter {
-		private final int value;
-
-		/**
-		 * Constructs a BooleanPlotter with that name and value.
-		 * @param name
-		 */
-		public IntPlotter(String name, int value) {
-			super (name);
-			this.value = value;
-		}
-
-		@Override
-		public int getValue() {
-			return value;
-		}
-	}
-
 	/**
 	 * Starts Metrics and adds the needed data.
 	 */
 	public static void start() {
 		try {
-			metrics = new Metrics(SkyblockExtension.inst());
-			
-			Graph errors = metrics.createGraph("ErrorHandler current count");
-			
-			errors.addPlotter(new Plotter("ErrorHandler current count") {
-				@Override
-				public int getValue() {
-					// TODO Auto-generated method stub
-					return ErrorHandler.getNumberOfCrashes();
-				}
-			});
-			
-			Graph errorTypes = metrics.createGraph("Error Type Distribution");
-			
-			//Contains each of the types of errors; full class name :3
-			Map<String, Integer> errorCounts = new HashMap<String, Integer>();
-			for (CrashReport c : ErrorHandler.errors) {
-				Integer i = errorCounts.get(c.getClass().getName());
-				if (i == null) {
-					i = 0;
-				}
-				i++;
-				//Once again, not sure if doing i++ will work.
-				errorCounts.put(c.getClass().getName(), i);
-			}
-			
-			//Now convert it to a set of plotters.
-			for (Map.Entry<String, Integer> entry : errorCounts.entrySet()) {
-				errorTypes.addPlotter(new IntPlotter(entry.getKey(), entry.getValue()));
-			}
-			
-			//WitherWarner text.
+			metrics = new Metrics(SkyblockExtension.inst());//WitherWarner text.
 			Graph witherWarnerEnabled = metrics.createGraph("Wither Warner Enabled");
 			
-			witherWarnerEnabled.addPlotter(
-					new BooleanPlotter("Wither Warner Enabled",
-							WitherWarner.enabled));
+			witherWarnerEnabled.addPlotter(new Plotter("Wither Warner Enabled") {
+				@Override
+				public int getValue() {
+					return WitherWarner.enabled ? 1 : 0;
+				}
+			});
 			
 			metrics.start();
 		} catch (IOException e) {
