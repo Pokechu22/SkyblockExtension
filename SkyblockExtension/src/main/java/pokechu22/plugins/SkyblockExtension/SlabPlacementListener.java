fb608cf01e84c34a1ec0ac4bb9fb1d519f624c85
Pokechu22 <Pokechu022@gmail.com>
2015-04-09 11:00:35 -0700

    Setting slab names on challenge complete now!
    Yay, ASkyBlock has a way to do this!

--- SkyblockExtension/src/main/java/pokechu22/plugins/SkyblockExtension/SlabPlacementListener.java
+++ SkyblockExtension/src/main/java/pokechu22/plugins/SkyblockExtension/SlabPlacementListener.java
@@ -1,9 +1,19 @@
 package pokechu22.plugins.SkyblockExtension;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.inventory.ItemFactory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import com.wasteofplastic.askyblock.events.ChallengeCompleteEvent;
 
 /**
  * Listens for the placement of certain blocks, and handles their behavior.
@@ -62,7 +72,7 @@ public class SlabPlacementListener implements Listener {
 	private static final int FULL_STONE_SLAB_2_ID = 181;
 	
 	/**
-	 * Replaces slabs with the proper variants.
+	 * Replaces placed slabs with the proper variants.
 	 * 
 	 * @param e
 	 */
@@ -80,6 +90,107 @@ public class SlabPlacementListener implements Listener {
 					e.getBlockPlaced().setTypeId(FULL_STONE_SLAB_2_ID);
 				}
 			}
+		}
+	}
+	
+	/**
+	 * Replaces slabs with proper variants in challenge rewards.
+	 * <br/>
+	 * Note that this requires ChallengeCompleteEvent to have already
+	 * given the reward items before the event fires -- as of right now
+	 * ASkyBlock does this but it may change in the future.
+	 *  
+	 * @param e
+	 */
+	@SuppressWarnings("deprecation")
+	@EventHandler(priority=EventPriority.LOWEST)
+	public void onChallengeComplete(ChallengeCompleteEvent e) {
+		ItemFactory itemFactory = Bukkit.getItemFactory();
+		
+		HashMap<Integer, ItemStack> items = new HashMap<>();
+		
+		items.putAll(e.getPlayer().getInventory().all(FULL_STONE_SLAB_1_ID));
+		items.putAll(e.getPlayer().getInventory().all(FULL_STONE_SLAB_2_ID));
+		items.putAll(e.getPlayer().getInventory().all(HALF_STONE_SLAB_1_ID));
+		items.putAll(e.getPlayer().getInventory().all(HALF_STONE_SLAB_2_ID));
+		
+		for (Map.Entry<Integer, ItemStack> item : items.entrySet()) {
+			ItemStack stack = item.getValue();
+			
+			//Replace any invalid slabs with the valid counterparts.
+			if (stack.getTypeId() == FULL_STONE_SLAB_1_ID) {
+				stack.setTypeId(HALF_STONE_SLAB_1_ID);
+			}
+			if (stack.getTypeId() == FULL_STONE_SLAB_2_ID) {
+				stack.setTypeId(HALF_STONE_SLAB_2_ID);
+			}
+			
+			if (stack.getTypeId() == HALF_STONE_SLAB_1_ID) {
+				ItemMeta meta;
+				if (stack.hasItemMeta()) {
+					meta = stack.getItemMeta();
+				} else {
+					meta = itemFactory.getItemMeta(stack.getType());
+				}
+				if (meta.hasDisplayName()) {
+					continue; //Abort updating the name.
+				}
+				switch (stack.getData().getData()) {
+				case 2: meta.setDisplayName(
+						"§rStone Wood Half Slab");
+					break;
+				case 8:
+					meta.setDisplayName("§rSuper Stone! " + 
+						"(Seamless Double Smooth Stone Half Slab)");
+					break;
+				case 9:
+					meta.setDisplayName(
+						"§rSeamless Double Sandstone Half Slab");
+					break;
+				case 10: meta.setDisplayName(
+						"§rDouble Stone Wood Half Slab");
+					break;
+				}
+				
+				meta.setLore(Arrays.asList(
+						"This is a special block that",
+						"1.8 doesn't like.  Sorry :/",
+						"But at least it works.",
+						"--Pokechu22"));
+				
+				stack.setItemMeta(meta);
+			}
+			if (stack.getTypeId() == HALF_STONE_SLAB_2_ID) {
+				ItemMeta meta;
+				if (stack.hasItemMeta()) {
+					meta = stack.getItemMeta();
+				} else {
+					meta = itemFactory.getItemMeta(stack.getType());
+				}
+				if (meta.hasDisplayName()) {
+					continue; //Abort updating the name.
+				}
+				switch (stack.getData().getData()) {
+				case 8:
+					meta.setDisplayName(
+							"§rSeamless Double Red Sandstone Half Slab");
+					break;
+				}
+				
+				meta.setLore(Arrays.asList(
+						"This is a special block that",
+						"1.8 doesn't like.  Sorry :/",
+						"But at least it works.",
+						"--Pokechu22"));
+				
+				stack.setItemMeta(meta);
+			}
+		}
+		
+		//Ensure that the inventory is updated (may be unneeded)
+		for (Map.Entry<Integer, ItemStack> item : items.entrySet()) {
+			e.getPlayer().getInventory().setItem(item.getKey(), 
+					item.getValue());
 		}
 	}
 }
